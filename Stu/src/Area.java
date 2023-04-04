74cacfd4fdfeaa2045f8b780427525d3269be4a0
costar011 <dnaer456@gmail.com>
2023-04-04 09:05:37 +0900

    72p

---
+++ Stu/src/Area.java
@@ -0,0 +1,17 @@
+
+public class Area {
+
+	public static void main(String[] args) {
+		// 72p
+		
+		final double PI = 3.14; // 원주율을 상수로 선언 
+		
+		double radius = 10.0; // 원의 반지름 
+		double Area = radius * radius * PI; // 원의 면적 계산 
+		
+		// 원의 면적을 화면에 출력한다 
+		System.out.println("원의 면적 = " + Area);
+
+	}
+
+}
