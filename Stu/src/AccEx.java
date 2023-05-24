e84adbfc7229f288950ad63d529fbdd14b3d4c25
costar011 <dnaer456@gmail.com>
2023-05-24 12:19:03 +0900

    x,y,z

---
+++ Stu/src/AccEx.java
@@ -0,0 +1,21 @@
+class AAA {
+	public int x;
+	private int y;
+	protected int z;
+	int w;
+}
+
+
+
+public class AccEx {
+
+	public static void main(String[] args) {
+		AAA ob = new AAA();
+		
+		ob.x = 10;
+		ob.z = 10;
+		ob.w = 10;
+
+	}
+
+}
