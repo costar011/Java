e43def80b63a528fd2da5ebe8b598550d974a27f
costar011 <dnaer456@gmail.com>
2023-05-24 11:44:18 +0900

    a, c, d

---
+++ Stu/src/AccessEx.java
@@ -0,0 +1,21 @@
+class Sample {
+	public int a;
+	private int b;
+	int c;
+	
+	protected int d;
+}
+
+
+public class AccessEx {
+
+	public static void main(String[] args) {
+		Sample aClass = new Sample();
+		
+		aClass.a = 10;
+		aClass.c = 10;
+		aClass.d = 10;
+
+	}
+
+}
