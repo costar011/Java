7b9ea1be6296188b29228f785e6d664f475a4fd9
costar011 <dnaer456@gmail.com>
2024-03-20 22:10:36 +0900

    최대공약

---
+++ Stu2/src/Mul.java
@@ -0,0 +1,13 @@
+
+public class Mul {
+
+	public static void main(String[] args) {
+		for(int i = 0; 12 < i; i++) {
+			if((i + 1) % 2 == 0) {
+				System.out.println("12의 최대공약수 :" + (i + 1));
+			}
+		}
+
+	}
+
+}
