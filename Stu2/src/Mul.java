5acde711852fdf8f1f89920c1e50f515cdcbf80c
costar011 <dnaer456@gmail.com>
2024-03-21 06:25:02 +0900

    최대공약수

--- Stu2/src/Mul.java
+++ Stu2/src/Mul.java
@@ -7,7 +7,13 @@ public class Mul {
 				System.out.println("12의 최대공약수 :" + (i + 1));
 			}
 		}
-
+		
+		for(int i = 0; 12 < i; i++) {
+			for(int j = 0; 6 < j; j++) {
+				if((j + 1) % 2 == 0 && (j+1) % 2 == 0) {
+					System.out.println("6과 12의 최대공약수 :" + (j + 1));
+				}
+			}
+		}
 	}
-
 }
