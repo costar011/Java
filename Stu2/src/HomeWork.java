fc5001d9f80f1f6998b04e7b16ec5cebce1e9e79
costar011 <dnaer456@gmail.com>
2024-03-22 22:29:37 +0900

    과

---
+++ Stu2/src/HomeWork.java
@@ -0,0 +1,17 @@
+class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+public class HomeWork {
+
+	public static void main(String[] args) {
+		// 이름을 메소드를 사용해서 출력
+
+	}
+
+}
