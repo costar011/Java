5f64092cc1fd381817a48bb5a0e5b6f3e394ee43
costar011 <dnaer456@gmail.com>
2024-03-22 23:42:13 +0900

    과제 1번

--- Stu2/src/HomeWork.java
+++ Stu2/src/HomeWork.java
@@ -1,17 +1,29 @@
-class Person {
-    private String name;
-    private int age;
-
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
+public class HomeWork {
+    public static void printName(String name) {
+        System.out.println("이름: " + name);
     }
 
-public class HomeWork {
+    public static void printDepartment(String department) {
+        System.out.println("학과: " + department);
+    }
 
-	public static void main(String[] args) {
-		// 이름을 메소드를 사용해서 출력
+    public static void printGrade(int grade) {
+        System.out.println("학년: " + grade);
+    }
 
-	}
+    public static void printStudentID(String studentID) {
+        System.out.println("학번: " + studentID);
+    }
 
+    public static void main(String[] args) {
+        String name = "정예림";
+        String department = "컴퓨터공학과";
+        int grade = 3;
+        String studentID = "2251029";
+
+        printName(name);
+        printDepartment(department);
+        printGrade(grade);
+        printStudentID(studentID);
+    }
 }
